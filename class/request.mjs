3e5b2cc371eb8037e04dd2fa93f9adcc5f312831
viktormarinho <viktormpcs@gmail.com>
2022-10-04 16:55:22 -0300

    ‼️ BREAKING: Remaking Request class logic to utilize new extractors. MIDDLEWARE STOPPED WORKING

--- class/request.mjs
+++ class/request.mjs
@@ -1,5 +1,6 @@
 import { responseWithError } from './../common/responseWithError.mjs'
 import http from 'http';
+import { Extractor } from './extractor.mjs';
 
 export class Request {
     /**
@@ -35,36 +36,9 @@ export class Request {
      * 
      * @param {http.IncomingMessage} req 
      */
-    extractParams(req) {
-        const _params = new URL(req.url, `http://${req.headers.host}`).searchParams
-        let params = {}
-        _params.forEach((val, key) => params[key] = val)
-        return params
-    }
-
-    /**
-     * 
-     * @param {http.IncomingMessage} req 
-     */
-    async extractBody(req) {
-        const buffers = [];
-
-        for await (const chunk of req) {
-            buffers.push(chunk)
-        }
-
-        const data = Buffer.concat(buffers).toString()
-
-        return JSON.parse(data)
-    }
-
-    /**
-     * 
-     * @param {http.IncomingMessage} req 
-     */
-    validateMiddlewares(req) {
+    validateMiddlewares(requestData) {
         for (let mw of this.middlewares) {
-            if (!mw.validate(req)) return { ok: false, middleware: mw }
+            if (!mw.validate(requestData)) return { ok: false, middleware: mw }
         }
         return { ok: true }
     }
@@ -77,27 +51,24 @@ export class Request {
      */
     async handleRequest(req, res) {
         if (!this.requestMethodMiddleware(req)) return this.rejectWrongMethod(req, res);
-        
-        const {ok, middleware} = this.validateMiddlewares(req);
 
-        if (!ok) {
-            return middleware.rejectFunction(req, res)
-        }
+        const params = Extractor.extractParams(req)
+        const headers = req.headers;
 
-        let params = this.extractParams(req)
-        
-        let any_err;
+        try {
+            const body = await Extractor.extractBody(req)
 
-        await this.extractBody(req)
-            .then(body => {
-                res.end(JSON.stringify(this.handlerFunction(params, body)))
-            })
-            .catch(err => {
-                any_err = err.message;
-            })
-        
-        if (any_err) {
-            return responseWithError(res, any_err, 400);
+            const requestData = {params, body, headers}
+
+            const {ok, middleware} = this.validateMiddlewares(requestData);
+
+            if (!ok) {
+                return middleware.rejectFunction(req, res)
+            }
+
+            res.end(JSON.stringify(this.handlerFunction(requestData)))
+        } catch (err) {
+            return responseWithError(res, err.message, 400);
         }
     }
 }
